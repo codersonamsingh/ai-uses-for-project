f08d56fa7591b343562fff95950524d73e1cee51
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-10-14 18:08:03 +0530

    page,jsx running

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+export default ({
+  
+    schema: './config/schema.js',
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://neondb_owner:Mjr5KaIfAgZ3@ep-jolly-unit-a5psheln.us-east-2.aws.neon.tech/ai-short-video-generator?sslmode=require',
+    },
+  });
